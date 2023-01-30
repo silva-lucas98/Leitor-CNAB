eaaee34e0654c0796b719104079d15ea3c791043
Lucas <lsengenheirocivil@gmail.com>
2023-01-29 22:53:12 -0300

    feat: save data on database

--- transactions/handle_uploaded_file.py
+++ transactions/handle_uploaded_file.py
@@ -1,6 +1,21 @@
+from .models import Transaction
+
+
 def handle_uploaded_file(request):
     uploaded_file = request.FILES["file"]
 
     for row in uploaded_file:
         string = row.decode()
-        print(string)
+        data = {
+            "type": string[0],
+            "date": string[1:9],
+            "value": string[9:19],
+            "cpf": string[19:30],
+            "card": string[30:42],
+            "hour": string[42:48],
+            "owner": string[48:62],
+            "store": string[62:80],
+        }
+
+        instance = Transaction.objects.create(**data)
+        instance.save()
